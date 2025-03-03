a7071ea2bd1939b1e84ffa85fed4350102e32be2
Martín Cura <martincura@gmail.com>
2025-03-03 01:48:49 +0000

    fix: blueground provider

--- api/quieromudarme/providers/blueground.py
+++ api/quieromudarme/providers/blueground.py
@@ -6,7 +6,7 @@ import math
 import re
 from datetime import date
 from decimal import Decimal
-from typing import Annotated, Any, Final, Literal
+from typing import Any, Final, Literal
 
 import niquests
 import pydantic as pc
@@ -15,12 +15,15 @@ import tenacity
 from quieromudarme import db
 from quieromudarme.logging import setup_logger
 from quieromudarme.providers.base import Currency, HousingPost, ProviderName
+from quieromudarme.providers.common import gen_user_agent
 from quieromudarme.utils import run_async_in_thread
 
 logger = setup_logger()
 
 
 BASE_URL: Final = "https://www.theblueground.com/pt-br/"
+MAX_PAGE_SIZE: Final = 50
+
 
 name: Final = ProviderName.BLUEGROUND
 
@@ -33,10 +36,12 @@ class BluegroundHousingPost(HousingPost):
     provider: ProviderName = ProviderName.BLUEGROUND
     code: str
     post_id: str = pc.Field(validation_alias=pc.AliasPath("id"))
+    publisher_id: str = pc.Field(validation_alias=pc.AliasPath("source"))
     path: str
+    url: str = ""  # URL is not present, it's generated
     title: str = pc.Field(validation_alias=pc.AliasPath("name"))
     lot_size: int = pc.Field(validation_alias=pc.AliasPath("lotSize"))
-    address: str = pc.Field(validation_alias=pc.AliasPath("address", "building"))
+    address_building: str | None = pc.Field(validation_alias=pc.AliasPath("address", "building"))
     special_price: bool = pc.Field(validation_alias=pc.AliasPath("specialPrice"))
     base_rent: Decimal = pc.Field(validation_alias=pc.AliasPath("baseRent", "amount"))
     base_rent_currency: Literal[Currency.EUR] = pc.Field(
@@ -46,22 +51,26 @@ class BluegroundHousingPost(HousingPost):
     geo_lat: float = pc.Field(validation_alias=pc.AliasPath("address", "lat"))
     geo_lng: float = pc.Field(validation_alias=pc.AliasPath("address", "lng"))
     bedrooms: int
-    floor: int = pc.Field(validation_alias=pc.AliasPath("highestFloor"))
+    floor: int | None = pc.Field(default=None, validation_alias=pc.AliasPath("highestFloor"))
     price: Decimal = pc.Field(validation_alias=pc.AliasPath("rent", "amount"))
     price_currency: Literal[Currency.EUR] = pc.Field(
         validation_alias=pc.AliasPath("rent", "currency")
     )
-    check_in_date: date = pc.Field(validation_alias=pc.AliasPath("rent", "minDuration", "start"))
-    check_out_date: date = pc.Field(validation_alias=pc.AliasPath("rent", "minDuration", "end"))
+    check_in_date: date | None = pc.Field(
+        default=None, validation_alias=pc.AliasPath("rent", "minDuration", "start")
+    )
+    check_out_date: date | None = pc.Field(
+        default=None, validation_alias=pc.AliasPath("rent", "minDuration", "end")
+    )
     available_from: date = pc.Field(validation_alias=pc.AliasPath("availableFrom"))
 
-    @pc.computed_field
-    @property
-    def url(self) -> str:
+    @pc.field_validator("url")
+    @classmethod
+    def validate_url(cls, v: str) -> str:
         """Merge base URL with `path`."""
-        return f"{BASE_URL}{self.path}"
+        return f"{BASE_URL}{v}"
 
-    @pc.computed_field
+    @pc.computed_field  # type: ignore [prop-decorator]
     @property
     def url_with_dates(self) -> str:
         """Merge base URL with `path` and `available_from`."""
@@ -82,25 +91,19 @@ class BluegroundHousingPost(HousingPost):
 class BluegroundSearchResult(pc.BaseModel):
     """Search results from Blueground from JSON-in-HTML."""
 
-    items: list[
-        Annotated[
-            BluegroundHousingPost,
-            pc.Field(validation_alias=pc.AliasPath("properties", "allProperties", "main")),
-        ]
-    ]
+    items: list[BluegroundHousingPost] = pc.Field(
+        validation_alias=pc.AliasChoices(
+            pc.AliasPath("properties", "allProperties", "main"), pc.AliasPath("properties", "main")
+        )
+    )
     items_per_page: int = pc.Field(
-        validation_alias=pc.AliasPath("configuration", "propertiesPerPage")
+        default=MAX_PAGE_SIZE, validation_alias=pc.AliasPath("configuration", "propertiesPerPage")
     )
-    items_up_to_page: int = pc.Field(
-        validation_alias=pc.AliasPath("properties", "meta", "loadedItems")
+    total_items: int = pc.Field(
+        validation_alias=pc.AliasChoices(
+            pc.AliasPath("properties", "meta", "totalItems"), pc.AliasPath("totalItems")
+        )
     )
-    total_items: int = pc.Field(validation_alias=pc.AliasPath("properties", "meta", "totalItems"))
-
-    @pc.computed_field
-    @property
-    def has_next_page(self) -> bool:
-        """Whether there is a next page of results."""
-        return self.items_up_to_page < self.total_items
 
 
 @tenacity.retry(
@@ -108,35 +111,56 @@ class BluegroundSearchResult(pc.BaseModel):
     stop=tenacity.stop_after_attempt(3),
     before_sleep=tenacity.before_sleep_log(logger, logging.WARNING),
 )
-def _fetch_page_async(url: str, *, page: int | None = None) -> BluegroundSearchResult:
-    """Fetch a page of Blueground search results, async."""
-    headers = {}
-    params = {"page": page} if page else {}
+async def _fetch_page_async_html(url: str, *, page: int | None = None) -> BluegroundSearchResult:
+    """Fetch a page of Blueground search results from HTML, async."""
+    headers = {"User-Agent": gen_user_agent()}
+    params = {"page": str(page)} if page else {}
 
     logger.debug(f"Requesting Blueground URL {url}, with params {params}")
     resp = niquests.get(url=url, params=params, headers=headers, timeout=20)
-
     resp.raise_for_status()
+
     if resp.text is None:
         msg = "No response text"
         raise ValueError(msg)
 
     json_match = re.search(
-        r"<script>window\.__INITIAL_STATE__ = (.*?)</script>", resp.text, re.DOTALL
+        r"<script>window\.__INITIAL_STATE__ *= *(.*?)</script>", resp.text, re.DOTALL
     )
     if json_match is None:
         msg = "Could not find JSON data in HTML response"
         raise ValueError(msg)
 
     json_data: str = json_match.group(1).strip()
+    # with open("resp.json", "w") as f:
+    #     f.write(json.dumps(json.loads(json_data), indent=2))
     return BluegroundSearchResult.model_validate_json(json_data)
+
+
+@tenacity.retry(
+    wait=tenacity.wait_exponential(multiplier=1, min=6, max=30),
+    stop=tenacity.stop_after_attempt(3),
+    before_sleep=tenacity.before_sleep_log(logger, logging.WARNING),
+)
+async def _fetch_page_async(url: str, *, page: int | None = None) -> BluegroundSearchResult:
+    """Fetch a page of Blueground search results from JSON, async."""
+    headers = {"User-Agent": gen_user_agent()}
+    params = {"items": str(MAX_PAGE_SIZE), "offset": str((page or 0) * MAX_PAGE_SIZE)}
+
+    logger.debug(f"Requesting Blueground URL {url}, with params {params}")
+    resp = niquests.get(url=url, params=params, headers=headers, timeout=20)
+    resp.raise_for_status()
+
+    if (data := resp.json()) is None:
+        msg = "No JSON data in response"
+        raise ValueError(msg)
+
+    return BluegroundSearchResult.model_validate(data)
 
 
 def is_valid_search_url(url: str) -> bool:
     """Return whether the URL is a valid search URL for Blueground."""
-    return bool(
-        re.match(r"http(s?)://www\.theblueground\.com/[a-zA-Z-]+/furnished-apartments-.+", url)
-    )
+    return bool(re.match(r"http(s?)://www\.theblueground\.com/api/furnished-apartments-.+", url))
 
 
 async def _gather_async_pages(url: str, pages: list[int]) -> list[BluegroundSearchResult]:
@@ -153,13 +177,17 @@ def get_search_results(
 
     first_page_results: BluegroundSearchResult = run_async_in_thread(_fetch_page_async(url))
     posts: list[HousingPost] = list(first_page_results.items)
-    logger.info("Found %d results", len(first_page_results.total_items))
+    logger.info("Found %d results", first_page_results.total_items)
 
-    last_page_num = min(
-        max_pages, math.ceil(first_page_results.total_items / first_page_results.items_per_page)
+    last_page_num = int(
+        min(
+            max_pages if max_pages is not None else float("inf"),
+            math.ceil(first_page_results.total_items / first_page_results.items_per_page),
+        )
     )
-    rest_page_nums = range(2, last_page_num + 1)
-    rest_page_results = run_async_in_thread(_gather_async_pages(url, rest_page_nums))
+    rest_page_results = run_async_in_thread(
+        _gather_async_pages(url, list(range(2, last_page_num + 1)))
+    )
     for page_result in rest_page_results:
         posts.extend(page_result.items)
 
@@ -174,7 +202,20 @@ def fetch_latest_results(
     search: db.GetHousingSearchesResult, *, max_pages: int | None = None
 ) -> list[HousingPost]:
     """Fetch latest results for a Blueground search."""
-    first_page_url = search.url.replace("page=[0-9]+&?", "")
+    first_page_url = search.url
     total_results, posts = get_search_results(first_page_url, payload=None, max_pages=max_pages)
     logger.info("Fetched %d posts from %d total results", len(posts), total_results)
     return posts
+
+
+if __name__ == "__main__":
+    """For debugging."""
+    import sys
+
+    if len(sys.argv) < 2:  # noqa: PLR2004
+        logger.error(f"Usage: {sys.argv[0]} URL")
+        sys.exit(1)
+
+    posts = get_search_results(sys.argv[1], payload=None)[1]
+    for post in posts:
+        logger.info(post.model_dump_json(indent=2))
